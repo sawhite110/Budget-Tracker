f6df0e7182b7835886f72f4b6a3e18665bba9b1b
SAW2Codes <stevenson.whiteii@gmail.com>
2021-06-25 20:52:44 -0400

    Updated service-worker.js FILES_TO_CACHE.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
   '/index.js',
   '/indexedDB.js',
   '/manifest.webmanifest',
+  '/service-worker.js',
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
